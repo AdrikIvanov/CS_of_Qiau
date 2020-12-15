1b720f6398868a697863a39ebe176b6491dc4343
AdrikIvanov <adrikivasub@gmail.com>
2020-12-15 22:27:43 +0330

    first sample of future homework added

---
+++ KalhorClass/HOMEWORKs/E3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Enter a number : ";
+    cin >> x;
+    for(int i=0;i<x;i++)
+    {
+        for(int j=x-1;j>i;j--)
+            cout << "-";
+        for(int n=1;n<i+2;n++)
+            cout << n;
+        cout << endl;
+    }
+    getch();
+}
