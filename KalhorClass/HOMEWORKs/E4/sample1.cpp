e04e6a03c049823ae8746d516b61e2e13288816f
mehdies-tech <myx42jtop@gmail.com>
2020-12-25 16:36:14 +0330

    sample1.cpp added.

---
+++ KalhorClass/HOMEWORKs/E4/sample1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    int x[10];
+    for(int i=0;i<10;i++){
+        cout<< "Enter x[" << i << "] : ";
+        cin>>x[i];
+    }
+    for(int i=0;i<10;i++)
+        if(100<=x[i] && x[i]<1000)
+            cout<<x[i]<<"\t";
+    getch();
+}
