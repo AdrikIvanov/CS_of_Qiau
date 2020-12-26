9869b9f7c98728ef1d58f022a75ee09677ad6cd1
MM010 <m.solee.ms40@gmail.com>
2020-12-26 12:02:41 +0330

    new sample for array

--- KalhorClass/HOMEWORKs/Next week/sample2.cpp
+++ KalhorClass/HOMEWORKs/Next week/sample2.cpp
@@ -1,17 +1,20 @@
+// Mohammad Mahdi Soleimani nia
+// Ostad kalhor
 #include <iostream>
 #include <conio.h>
 using namespace std;
 int main (){
 int x[10],flag=0,i;
-    cout << "enter numbers= ";
- for (i=0 ; i<=9 ; i++)
-    cin >> x[i];
-    cout << "result= ";
- for(i=0 ; i<=9 ; i++)
-    if(x[i]>100 && x[i]<1000){
-        flag=1;
-        cout <<x[i]<<" , " << endl << "please enter to exsit...";}
-    if(flag==0){
-        cout << "error!"<< endl << "please enter to exsit...";}
+ cout << "write 10 number= ";
+   for (i=0 ; i<=9 ; i++)
+      cin >> x[i];
+      cout << "result= ";
+   for(i=0 ; i<=9 ; i++)
+      if(x[i]>100 && x[i]<1000){
+         flag=1;
+         cout <<x[i]<<" , ";}
+      if(flag==0){
+         cout << "error!";}
 getch();
 } 
+                                                            
