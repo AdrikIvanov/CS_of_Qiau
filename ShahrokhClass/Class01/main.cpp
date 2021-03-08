1cdb6fce77bca9a1314d5f81cf6071db860cc68c
mehdies-tech <myx42jtop@gmail.com>
2021-03-08 13:26:14 +0330

    Class01 added

---
+++ ShahrokhClass/Class01/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+
+int f(int n){
+    if (n <= 0)
+        return -1;
+    int cnt=0;
+    while(n!=1){
+        if(n%2==0)
+            n= n/2;
+        else
+            n=(n*3) + 1;
+        cnt++;
+    }
+    return cnt;
+}
+
+int main(){
+    int number;
+    cout << "Enter a number : ";
+    cin >> number;
+    cout << "Result : " << f(number);
+    cin.get();
+    cin.get();
+    return 0;
+}
